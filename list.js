0d9265745430f981b724ea4c4df8fced892c6411
vbrawl <greninjagr@gmail.com>
2024-02-05 21:08:00 +0200

    Added basic list tag.

---
+++ list.js
@@ -0,0 +1,95 @@
+
+
+(function(fwk) {
+
+    class fwk_walker {
+        constructor(lst) {
+            this.lst = lst;
+            this.legs = [null, null];
+            this.prevStep = 1;
+        }
+
+        doubleStep() {
+            this.singleStep();
+            this.singleStep();
+        }
+
+        singleStep() {
+            var currentStep = this.getCurrentStep();
+            if(this.legs[this.prevStep] === null) { this.legs[currentStep] = this.lst.children[0]; }
+            else { this.legs[currentStep] = this.legs[this.prevStep].nextElementSibling; }
+            this.prevStep = currentStep;
+            return this.legs[this.prevStep];
+        }
+
+        getCurrentStep() {
+            var s = this.prevStep + 1;
+            if(s >= this.legs.length) { s = 0; }
+            return s;
+        }
+    }
+
+    class fwk_list extends HTMLDivElement {
+        static observedAttributes = [
+            "orientation",
+            "spacing",
+            "border"
+        ];
+
+        fixOrientation(o) {
+            if(o === 'horizontal' || o === 'vertical') { return; }
+            this.setAttribute("orientation", "vertical");
+        }
+
+        setSpacing(s) {
+            this.style.setProperty("--fwk-list-spacing", s);
+        }
+
+        setBorder(b) {
+            this.style.setProperty("--fwk-list-border", b);
+        }
+
+        addSpacers() {
+            var walker = new fwk_walker(this);
+            for(walker.doubleStep(); walker.legs[0] !== null && walker.legs[1] !== null; walker.singleStep()) {
+                if(!walker.legs[0].classList.contains("fwk-list-spacer") && !walker.legs[1].classList.contains("fwk-list-spacer")) {
+                    let d = document.createElement("div");
+                    d.classList.add("fwk-list-spacer");
+                    this.insertBefore(d, walker.legs[walker.prevStep]);
+                }
+            }
+        }
+
+        connectedCallback() {
+            this.fixOrientation(this.getAttribute("orientation"));
+
+            {
+                let spacing = this.getAttribute("spacing");
+                this.setSpacing(spacing);
+                this.addSpacers();
+            }
+        }
+
+        attributeChangedCallback(name, oval, nval) {
+            switch (name) {
+                case "orientation":
+                    this.fixOrientation(nval);
+                    break;
+            
+                case "spacing":
+                    this.setSpacing(nval);
+                    break;
+
+                case "border":
+                    this.setBorder(nval);
+                    break;
+
+                default:
+                    console.error(`No handler for attribute "${name}" in class "${this.name}"`);
+                    break;
+            }
+        }
+    }
+
+    customElements.define("fwk-list", fwk_list, {extends: "div"});
+}(window.fwk = window.fwk || {}));
