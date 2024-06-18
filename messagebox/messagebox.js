4bbc75fb3ba5fdb717f1f261d70dba73b88f12d4
Vbrawl <greninjagr@gmail.com>
2024-06-18 14:40:54 +0300

    Changed messagebox functions to use hwk.Popup.

--- messagebox/messagebox.js
+++ messagebox/messagebox.js
@@ -2,135 +2,145 @@
 
 
 (function(hwk) {
+    /**
+     * TemplateObject = {
+     *  tag: 'div',
+     *  classes: ['class1', 'class2'],
+     *  text: 'Lorem Ipsum',
+     *  events: {click: () => {}}
+     * }
+     * 
+     * @param {Array<Object>} templates Where object is described above as `TemplateObject`
+     * @returns {hwk.Popup}
+     */
+    hwk.createMessageBox = async function(templates) {
+        await customElements.whenDefined("hwk-popup");
 
-    hwk.MessageBox = class extends HTMLDivElement {
-        constructor() {
-            super();
-            this.setAttribute("is", "hwk-messagebox");
+        const msg_box = new hwk.Popup();
+        msg_box.setAttribute("hwk-popup-autoclose", "false");
+        msg_box.classList.add("hwk-messagebox");
 
-            const panel = document.createElement("div");
-            panel.classList.add("hwk-messagebox-panel");
-            while(this.childNodes.length != 0) {
-                panel.appendChild(this.childNodes[0]);
+        for (let i = 0; i < templates.length; i++) {
+            const template = templates[i];
+            const element = document.createElement(template.tag);
+            element.classList.add(...template.classes);
+            element.innerText = template.text;
+
+            if(template.events) {
+                for (const eventName in template.events) {
+                    if (Object.hasOwnProperty.call(template.events, eventName)) {
+                        const eventHandler = template.events[eventName];
+                        element.addEventListener(eventName, eventHandler);
+                    }
+                }
             }
-            this.appendChild(panel);
+
+            msg_box.appendChild(element);
         }
+
+        return msg_box;
     }
 
 
     hwk.alert = async function(message) {
-        return await new Promise((resolve, reject) => {
-            try {
-                const msg_box = new hwk.MessageBox();
-                const panel = msg_box.getElementsByClassName("hwk-messagebox-panel")[0];
-                const text_widget = document.createElement("div");
-                const ok_button = document.createElement("button");
+        return await new Promise(async (resolve) => {
+            const msg_box = await hwk.createMessageBox([{
+                tag: "div",
+                classes: ["hwk-messagebox-text"],
+                text: message
+            }, {
+                tag: "button",
+                classes: ["hwk-messagebox-ok-button"],
+                text: "Ok",
+                events: {
+                    click: () => {
+                        msg_box.remove();
+                        resolve();
+                    }
+                }
+            }]);
 
-                text_widget.classList.add("hwk-messagebox-alert-text");
-                text_widget.innerText = message;
-
-                ok_button.classList.add("hwk-messagebox-alert-ok-button");
-                ok_button.innerText = "Ok";
-
-                panel.appendChild(text_widget);
-                panel.appendChild(ok_button);
-                document.body.appendChild(msg_box);
-
-                ok_button.addEventListener("click", () => {
-                    msg_box.remove();
-                    resolve();
-                });
-            }
-            catch(e) { reject(e); }
+            document.body.appendChild(msg_box);
         });
     }
 
     hwk.confirm = async function(message) {
-        return await new Promise((resolve, reject) => {
-            try {
-                const msg_box = new hwk.MessageBox();
-                const panel = msg_box.getElementsByClassName("hwk-messagebox-panel")[0];
-                const text_widget = document.createElement("div");
-                const ok_button = document.createElement("button");
-                const cancel_button = document.createElement("button");
-
-                msg_box.classList.add("hwk-messagebox-confirm");
-
-                text_widget.classList.add("hwk-messagebox-confirm-text");
-                text_widget.innerText = message;
-
-                ok_button.classList.add("hwk-messagebox-confirm-ok-button");
-                ok_button.innerText = "Ok";
-
-                cancel_button.classList.add("hwk-messagebox-confirm-cancel-button");
-                cancel_button.innerText = "Cancel";
-
-                panel.appendChild(text_widget);
-                panel.appendChild(ok_button);
-                panel.appendChild(cancel_button);
-                document.body.appendChild(msg_box);
-
-
-                ok_button.addEventListener("click", () => {
-                    msg_box.remove();
-                    resolve(true);
-                });
-                cancel_button.addEventListener("click", () => {
-                    msg_box.remove();
-                    resolve(false);
-                })
-            }
-            catch(e) { reject(e); }
-        })
-    }
-
-    hwk.prompt = async function(message) {
-        return await new Promise((resolve, reject) => {
-            try {
-                const msg_box = new hwk.MessageBox();
-                const panel = msg_box.getElementsByClassName("hwk-messagebox-panel")[0];
-                const text_widget = document.createElement("div");
-                const text_input = document.createElement("input");
-                const ok_button = document.createElement("button");
-                const cancel_button = document.createElement("button");
-    
-                msg_box.classList.add("hwk-messagebox-prompt");
-    
-                text_widget.classList.add("hwk-messagebox-prompt-text");
-                text_widget.innerText = message; // enter = ok
-    
-                text_input.classList.add("hwk-messagebox-prompt-input");
-    
-                ok_button.classList.add("hwk-messagebox-prompt-ok-button");
-                ok_button.innerText = "Ok"; // input value
-    
-                cancel_button.classList.add("hwk-messagebox-prompt-cancel-button");
-                cancel_button.innerText = "Cancel"; // null
-    
-                panel.appendChild(text_widget);
-                panel.appendChild(text_input);
-                panel.appendChild(ok_button);
-                panel.appendChild(cancel_button);
-                document.body.appendChild(msg_box);
-    
-                const ok_handler = () => {
-                    const input_value = text_input.value;
-                    msg_box.remove();
-                    resolve(input_value);
+        return await new Promise(async (resolve) => {
+            const msg_box = await hwk.createMessageBox([{
+                tag: "div",
+                classes: ["hwk-messagebox-text"],
+                text: message,
+            }, {
+                tag: "button",
+                classes: ["hwk-messagebox-ok-button"],
+                text: "Ok",
+                events: {
+                    click: () => {
+                        msg_box.remove();
+                        resolve(true);
+                    }
                 }
-                ok_button.addEventListener("click", ok_handler);
-                text_input.addEventListener("keypress", (evt) => {
-                    if(evt.key === "Enter" && !evt.shiftKey) { ok_handler(); }
-                });
-    
-                cancel_button.addEventListener("click", () => {
-                    msg_box.remove();
-                    resolve(null);
-                });
-            }
-            catch(e) { reject(e); }
+            }, {
+                tag: "button",
+                classes: ["hwk-messagebox-cancel-button"],
+                text: "Cancel",
+                events: {
+                    click: () => {
+                        msg_box.remove();
+                        resolve(false);
+                    }
+                }
+            }]);
+
+            msg_box.classList.add("confirm");
+            document.body.appendChild(msg_box);
         });
     }
 
-    customElements.define("hwk-messagebox", hwk.MessageBox, {extends: "div"});
+    hwk.prompt = async function(message) {
+        return await new Promise(async (resolve) => {
+            const msg_box = await hwk.createMessageBox([{
+                tag: "div",
+                classes: ["hwk-messagebox-text"],
+                text: message
+            }, {
+                tag: "input",
+                classes: ["hwk-messagebox-input"],
+                text: "",
+                events: {
+                    keypress: (evt) => {
+                        if(evt.key === "Enter" && !evt.shiftKey) {
+                            const input_value = msg_box.getElementsByClassName("hwk-messagebox-input")[0].value;
+                            msg_box.remove();
+                            resolve(input_value);
+                        }
+                    }
+                }
+            }, {
+                tag: "button",
+                classes: ["hwk-messagebox-ok-button"],
+                text: "Ok",
+                events: {
+                    click: () => {
+                        const input_value = msg_box.getElementsByClassName("hwk-messagebox-input")[0].value;
+                        msg_box.remove();
+                        resolve(input_value);
+                    }
+                }
+            }, {
+                tag: "button",
+                classes: ["hwk-messagebox-cancel-button"],
+                text: "Cancel",
+                events: {
+                    click: () => {
+                        msg_box.remove();
+                        resolve(null);
+                    }
+                }
+            }]);
+
+            msg_box.classList.add("prompt");
+            document.body.appendChild(msg_box);
+        });
+    }
 }(window.hwk = window.hwk || {}));
